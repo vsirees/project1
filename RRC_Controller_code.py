800d045d58e3d9d9d1101bb52e49c0297d10e574
vsirees <116018442+vsirees@users.noreply.github.com>
2023-01-16 22:11:44 +0530

    code updated on 16-01-23

--- RRC_Controller_code.py
+++ RRC_Controller_code.py
@@ -55,12 +55,57 @@ df1['previous_system_frame_number']=df1['previous_system_frame_number'].fillna(0
 df1.dtypes
 
 #applying condition " current line system frame number is less then previous system frame number "
-df1['system_frame_number'] =np.where(df1['current_system_frame_number'] <= df1['previous_system_frame_number'],1,0)
+df1['system_frame_number'] =np.where(df1['current_sys	tem_frame_number'] <= df1['previous_system_frame_number'],1,0)
 
 #calculation
 df1["SCS_column"]=np.where((df1["system_frame_number"]==1) & 
                            (df1["Qualcomm 5G-NR MAC UL Physical Channel Schedule Report[Per Slot] PCell SCS"]=="15kHz"),
                            (10239-df1["previous_frameslot"].astype('int')+df1["current_frameslot"].astype('int')),df1["system_frame_number"])
 
+#calculation
+df1["SCS_column"]=np.where((df1["system_frame_number"]==1) & 
+                           (df1["Qualcomm 5G-NR MAC UL Physical Channel Schedule Report[Per Slot] PCell SCS"]=="30kHz"),
+                           (102319-df1["previous_frameslot"].astype('int')+df1["current_frameslot"].astype('int')),df1["system_frame_number"])
+
+
+#calculation
+df1["SCS_column"]=np.where((df1["system_frame_number"]==1) & 
+                           (df1["Qualcomm 5G-NR MAC UL Physical Channel Schedule Report[Per Slot] PCell SCS"]=="120kHz"),
+                           (102379-df1["previous_frameslot"].astype('int')+df1["current_frameslot"].astype('int')),df1["system_frame_number"])
+
+
 #exporting file
 df1.to_csv(r"C:\Users\siriv\OneDrive\Desktop\project1\S22_MOBILITY_DL_ITER5_v1.csv",index=True)
+
+
+# current_slot_number_column
+df1['current_slot_number']=df1['Qualcomm 5G-NR MAC UL Physical Channel Schedule Report[Per Slot] PCell Slot Number'].shift(0)
+
+# previous_slot_number_column
+df1['previous_slot_number']=df1['current_slot_number'].shift(1)
+
+#filling missing values with zero
+df1['current_slot_number']=df1['current_slot_number'].fillna(0).astype('int')
+df1['previous_slot_number']=df1['previous_slot_number'].fillna(0).astype('int')
+
+
+#current & previous slot numbers difference
+df1['slot_number_diff']=df1['current_slot_number']-df1['previous_slot_number']
+
+#calculation
+df1["slot_number"]=np.where(df1["slot_number_diff"]<=9&(df1["Qualcomm 5G-NR MAC UL Physical Channel Schedule Report[Per Slot] PCell SCS"]=="15kHz"),1,0)
+df1["slot_number"]=np.where(df1["slot_number_diff"]<=19&(df1["Qualcomm 5G-NR MAC UL Physical Channel Schedule Report[Per Slot] PCell SCS"]=="30kHz"),1,0)
+df1["slot_number"]=np.where(df1["slot_number_diff"]<=79&(df1["Qualcomm 5G-NR MAC UL Physical Channel Schedule Report[Per Slot] PCell SCS"]=="120kHz"),1,0)
+
+
+
+
+
+
+
+
+
+
+
+
+
